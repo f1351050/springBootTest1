9cc528dcd6bd5cc95a20bc5908c617fd6f374b95
f1351050 <f1351050@gmail.com>
2023-12-05 16:52:00 +0900

    first commit

---
+++ src/test/java/com/uhablog/SampaleWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uhablog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampaleWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
